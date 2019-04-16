face3f5a2aadccd6ab17cd7940daf66bab593a0d
edwin <camp.bbs@gmail.com>
2019-04-17 01:01:24 +1000

    change render html instead of text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-	  render text: "HI,How are you!"
+	  render html: "HI,How are you!"
   end
 end
